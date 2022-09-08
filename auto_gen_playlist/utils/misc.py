9b5b518e7d442832e708b14c643f2fee6d5d0686
Anthyme <90059514+derorsche@users.noreply.github.com>
2022-09-08 21:25:31 +0900

    不要になったモジュールを削除

--- auto_gen_playlist/utils/misc.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-
-def set_environment_var() -> None:
-    with open("lastfm-utils/config/config.json") as f:
-        vars = json.load(f)
-        for name, value in vars.items():
-            os.environ[name] = value
