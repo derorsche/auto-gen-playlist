ca7d622e174f5beedad968b4e1f412b02bf4603c
Anthyme <90059514+derorsche@users.noreply.github.com>
2022-09-08 19:25:04 +0900

    ログ出力にフィルターを追加

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import asyncio
-from logging import config
+from logging import Filter, LogRecord, config
 
+import dotenv
 from yaml import safe_load
 
 LOG_CONF_PATH = "etc/log-conf.yaml"
 
 
+class StreamHandlerFilter(Filter):
+    def filter(self, record: LogRecord):
+        return "auto_gen_playlist.requests" != record.name
+
+
 async def main():
+    dotenv.load_dotenv()  # type: ignore
+
     with open(LOG_CONF_PATH, "r", encoding="utf-8") as f:
         yml = safe_load(f)
     config.dictConfig(yml)
