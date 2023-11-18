6c45c950f790fa62a665aae06592edd7334b323e
Anthyme <90059514+derorsche@users.noreply.github.com>
2023-11-18 18:36:22 +0900

    Modify main.py per other changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import asyncio
 from logging import Filter, LogRecord, config
-from random import randint, sample
 
-import click
 import dotenv
 import spotipy
 from yaml import safe_load
 
-from auto_gen_playlist import spotify
-from auto_gen_playlist.core import generate_bimestrial_top_track_playlist
+from auto_gen_playlist.top_track import (
+    generate_bimestrial_top_track_playlist,
+    generate_recommended_playlist,
+)
 
 LOG_CONF_PATH = "etc/log-conf.yaml"
 SCOPE = (
@@ -22,8 +22,7 @@ class StreamHandlerFilter(Filter):
         return "auto_gen_playlist.lastfm.requests" != record.name
 
 
-@click.group()
-def cli():
+def load_config():
     dotenv.load_dotenv()  # type: ignore
 
     with open(LOG_CONF_PATH, "r", encoding="utf-8") as f:
@@ -31,60 +30,54 @@ def cli():
     config.dictConfig(yml)
 
 
-@click.command(name="top-track")
-def top_track():
-    year = int(input("year (required): "))
-    index = int(
-        input("index (required, 1 <= index <= 6, 1: Jan-Feb, 2: Mar-Apr, ...): ")
+async def main():
+    load_config()
+    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=SCOPE))
+
+    print(
+        "To create a recommended playlist, enter a playlist URL.  To create a top-track playlist, press the enter key."  # noqa: E501
     )
-    if res := input("number of songs (optional, default: 45): "):
-        count = int(res)
+    url = input("... ")
+    print()
+
+    if url:
+        while True:
+            print("Enter the index of the seed song.")
+            idx = int(input("... "))
+            generate_recommended_playlist(sp, url, idx)
+            print()
+
+            print("To create another recommended playlist, enter a playlist URL.")
+            url = input("... ")
+            print()
+
     else:
-        count = 45
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(top_track_core(year, index, count))
+        print("To delete tha cache and re-fetch the scrobbles, enter 'T'.")
+        if "T" == input("... "):
+            refetch = True
+        else:
+            refetch = False
+        print()
 
+        print("To delete tha previous auto-generated top-track playlists, enter 'T'.")
+        if "T" == input("... "):
+            update_old = True
+        else:
+            update_old = False
+        print()
 
-async def top_track_core(year: int, index: int, count: int):
-    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=SCOPE))
-    await generate_bimestrial_top_track_playlist(sp, year, index, count)
+        print("To specify the starting point, enter the year.")
+        if year := input("... "):
+            year = int(year)
+        else:
+            year = None
+        print()
 
+        await generate_bimestrial_top_track_playlist(
+            sp, refetch=refetch, update_old=update_old, since_year=year
+        )
 
-@click.command()
-def reorder():
-    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=SCOPE))
-    url = input("playlist url (required): ")
-    if input("newly create playlist? (y/n): ") == "y":
-        duplicate = True
-    else:
-        duplicate = False
-    fts = [f for f in spotify.Features]
-    print(", ".join([f"{cnt}: {e.value}" for cnt, e in enumerate(fts)]))
-    feature = fts[int(input("specify the feature to reorder by (required, int): "))]
-    spotify.reorder_playlist_by_features(sp, url, feature, duplicate)
-
-
-@click.command()
-def playlist():
-    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=SCOPE))
-    url = input("base playlist url (required): ")
-    if res := input("seed song index (optional, 1-indexed): "):
-        idx = int(res) - 1
-    else:
-        idx = None
-    fts = [f for f in spotify.Features]
-    print(", ".join([f"{cnt}: {e.value}" for cnt, e in enumerate(fts)]))
-    if res := input("specify the features (optional, int, white-space separated): "):
-        features = [fts[int(i)] for i in res.split()]
-    else:
-        features = sample(fts, randint(1, 3))
-
-    spotify.generate_recommendation_playlist(sp, url, features, idx=idx, strict=True)
-
-
-cli.add_command(top_track)
-cli.add_command(reorder)
-cli.add_command(playlist)
 
 if __name__ == "__main__":
-    cli()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
